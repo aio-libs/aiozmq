7c7f673eeb731baf186db66a26147b67b8762eae
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-03-27 22:53:15 +0200

    Add example for subhandlers of AttrHandler

---
+++ examples/rpc_with_subhanlers.py
@@ -0,0 +1,54 @@
+import asyncio
+import aiozmq
+import aiozmq.rpc
+
+
+class Handler(aiozmq.rpc.AttrHandler):
+
+    def __init__(self, ident):
+        self.ident = ident
+        self.subhandler = SubHandler(self.ident, 'subident')
+
+    @aiozmq.rpc.method
+    def a(self):
+        return (self.ident, 'a')
+
+
+class SubHandler(aiozmq.rpc.AttrHandler):
+
+    def __init__(self, ident, subident):
+        self.ident = ident
+        self.subident = subident
+
+    @aiozmq.rpc.method
+    def b(self):
+        return (self.ident, self.subident, 'b')
+
+
+@asyncio.coroutine
+def go():
+    server = yield from aiozmq.rpc.start_server(
+        Handler('ident'), bind='tcp://*:*')
+    server_addr = next(iter(server.transport.bindings()))
+
+    client = yield from aiozmq.rpc.open_client(
+        connect=server_addr)
+
+    ret = yield from client.rpc.a()
+    assert ('ident', 'a') == ret
+
+    ret = yield from client.rpc.subhandler.b()
+    assert ('ident', 'subident', 'b') == ret
+
+    server.close()
+    client.close()
+
+
+def main():
+    asyncio.set_event_loop_policy(aiozmq.ZmqEventLoopPolicy())
+    asyncio.get_event_loop().run_until_complete(go())
+    print("DONE")
+
+
+if __name__ == '__main__':
+    main()
