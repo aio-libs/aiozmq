c12b32867f44e77da3009396f85afacc35973c9f
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-05-28 09:33:33 +0300

    Change version to 0.4, update changes.txt

--- aiozmq/__init__.py
+++ aiozmq/__init__.py
@@ -12,7 +12,7 @@ __all__ = ('ZmqSelector', 'ZmqEventLoop', 'ZmqEventLoopPolicy',
            'ZmqTransport', 'ZmqProtocol',
            'version_info', 'version')
 
-__version__ = '0.4.0a1'
+__version__ = '0.4.0'
 
 version = __version__ + ' , Python ' + sys.version
 
