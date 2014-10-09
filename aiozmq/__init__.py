b86060438384c00f8c3d753ba30842206d6a7119
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-10-09 19:15:43 +0300

    Bump version

--- aiozmq/__init__.py
+++ aiozmq/__init__.py
@@ -13,7 +13,7 @@ __all__ = ('ZmqSelector', 'ZmqEventLoop', 'ZmqEventLoopPolicy',
            'create_zmq_connection',
            'version_info', 'version')
 
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 
 version = __version__ + ' , Python ' + sys.version
 
