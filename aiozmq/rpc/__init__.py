5b1704a20d6384b3a14fb1db3d798150c715fe86
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-02-13 16:02:57 +0200

    Fix pep8 errors

--- aiozmq/rpc/__init__.py
+++ aiozmq/rpc/__init__.py
@@ -1,17 +1,9 @@
 """ZeroMQ RPC/Pipeline/PubSub services"""
 
-_MSGPACK_VERSION = (0, 4, 0)
-_MSGPACK_VERSION_STR = '.'.join(map(str, _MSGPACK_VERSION))
-
 try:
     from msgpack import version as msgpack_version
 except ImportError:  # pragma: no cover
     msgpack_version = (0,)
-
-if msgpack_version < _MSGPACK_VERSION:  # pragma: no cover
-    raise ImportError("aiozmq.rpc requires msgpack-python package"
-                      " (version >= {})".format(_MSGPACK_VERSION_STR))
-
 
 from .base import (
     method,
@@ -38,6 +30,14 @@ from .pubsub import (
     )
 
 from .log import logger
+
+_MSGPACK_VERSION = (0, 4, 0)
+_MSGPACK_VERSION_STR = '.'.join(map(str, _MSGPACK_VERSION))
+
+if msgpack_version < _MSGPACK_VERSION:  # pragma: no cover
+    raise ImportError("aiozmq.rpc requires msgpack-python package"
+                      " (version >= {})".format(_MSGPACK_VERSION_STR))
+
 
 __all__ = [
     'method',
