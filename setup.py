0d84c386479887f88a5b1c5e0df8d1b85152e555
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-09-20 10:45:55 +0300

    Python 3.5 compatible

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ classifiers = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Operating System :: POSIX',
     'Operating System :: MacOS :: MacOS X',
     'Operating System :: Microsoft :: Windows',
