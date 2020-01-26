f3b7aa24305e406c51200382bc9ae0e85f6476c7
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-01-26 20:48:28 +0200

    Release 0.9.0a0

--- aiozmq/__init__.py
+++ aiozmq/__init__.py
@@ -18,7 +18,7 @@ __all__ = ('ZmqSelector', 'ZmqEventLoop', 'ZmqEventLoopPolicy',
            'create_zmq_connection',
            'version_info', 'version')
 
-__version__ = '0.8.0'
+__version__ = '0.9.0a0'
 
 version = __version__ + ' , Python ' + sys.version
 
