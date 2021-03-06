c122dc14ca9ee726ac094dfd197baf8dbf38465b
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2021-03-06 07:44:52 -0800

    support Python 3.9

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ classifiers = [
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
     'Operating System :: POSIX',
     'Operating System :: MacOS :: MacOS X',
     'Operating System :: Microsoft :: Windows',
