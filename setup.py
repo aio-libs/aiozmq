5fdd27f2abbb0ea74c0e2c8f0377214e909a445e
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-10-31 07:26:44 -0700

    Support Python 3.11

--- setup.py
+++ setup.py
@@ -39,6 +39,8 @@ classifiers = [
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
     "Operating System :: POSIX",
     "Operating System :: MacOS :: MacOS X",
     "Operating System :: Microsoft :: Windows",
